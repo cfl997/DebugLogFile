c053d52c0e38646635296617b4d95b039025d5b0
CaoFulei <cfl997@163.com>
2024-06-18 12:40:19 +0800

    restore streamBuf

--- DebugLogFile.cpp
+++ DebugLogFile.cpp
@@ -13,7 +13,10 @@ namespace DebugLogFile
 	std::wfilebuf* g_pDebugFileBuf_w = nullptr; // std::wcout
 	std::filebuf* g_pDebugFileBuf = nullptr; // std::cout
 
-	void OpenDebugFile(const std::wstring& szFilePath)
+	std::wstreambuf* originalWcoutBuffer = nullptr; // 原始宽字符输出缓冲区
+	std::streambuf* originalCoutBuffer = nullptr; // 原始窄字符输出缓冲区
+
+	void OpenDebugFile(const std::wstring & szFilePath)
 	{
 		std::wstring strFileName;
 		if (szFilePath.empty()) {
@@ -30,7 +33,7 @@ namespace DebugLogFile
 
 		std::wstringstream ss;
 		ss << szFilePath;
-		ss << L"/ServerDebugOutput";
+		ss << L"/CGTIMERender2D";
 		ss << std::put_time(&timeInfo, L"%Y%m%d");
 		ss << L".log";
 		strFileName = ss.str();
@@ -42,8 +45,8 @@ namespace DebugLogFile
 			g_pDebugFileBuf_w = nullptr;
 			return;
 		}
-		// 重定向宽字符输出到文件
-		std::wstreambuf* originalWcoutBuffer = std::wcout.rdbuf();
+		// 保存原始的宽字符输出缓冲区并重定向
+		originalWcoutBuffer = std::wcout.rdbuf();
 		std::wcout.rdbuf(g_pDebugFileBuf_w);
 #endif //USE_WCOUT
 
@@ -54,8 +57,8 @@ namespace DebugLogFile
 			g_pDebugFileBuf = nullptr;
 			return;
 		}
-		// 重定向窄字符输出到文件
-		std::streambuf* originalCoutBuffer = std::cout.rdbuf();
+		// 保存原始的窄字符输出缓冲区并重定向
+		originalCoutBuffer = std::cout.rdbuf();
 		std::cout.rdbuf(g_pDebugFileBuf);
 
 		std::cout << timeInfo.tm_hour << ":" << timeInfo.tm_min << ":" << timeInfo.tm_sec << std::endl;
@@ -66,7 +69,10 @@ namespace DebugLogFile
 	{
 #ifdef USE_WCOUT
 		// 还原宽字符输出
-		std::wcout.rdbuf(std::wcerr.rdbuf());
+		if (originalWcoutBuffer) {
+			std::wcout.rdbuf(originalWcoutBuffer);
+			originalWcoutBuffer = nullptr;
+		}
 
 		if (g_pDebugFileBuf_w) {
 			g_pDebugFileBuf_w->close();
@@ -77,7 +83,10 @@ namespace DebugLogFile
 
 #ifdef USE_COUT
 		// 还原窄字符输出
-		std::cout.rdbuf(g_pDebugFileBuf);
+		if (originalCoutBuffer) {
+			std::cout.rdbuf(originalCoutBuffer);
+			originalCoutBuffer = nullptr;
+		}
 
 		if (g_pDebugFileBuf) {
 			g_pDebugFileBuf->close();
